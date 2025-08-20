9e0fbda93c31fae950c473414362f307dd4dc79f
Elis Eriksson <Spelis@duck.com>
2025-08-20 13:50:09 +0200

    Initial

---
+++ src/main/java/eu/projnull/spelis/valvesneak/Valvesneak.java
@@ -0,0 +1,10 @@
+package eu.projnull.spelis.valvesneak;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Valvesneak implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
